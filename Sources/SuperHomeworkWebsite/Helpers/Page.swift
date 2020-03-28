c9fb5a715572e2780ea34f3d7e0b6300538f8c59
Wilson Gramer <wgramer03@gmail.com>
2020-03-27 21:00:55 -0400

    Add Google Analytics

--- Sources/SuperHomeworkWebsite/Helpers/Page.swift
+++ Sources/SuperHomeworkWebsite/Helpers/Page.swift
@@ -26,9 +26,13 @@ extension Page {
                     .title("\(self.title) — SuperHomework"),
                     .stylesheet("https://fonts.googleapis.com/css?family=Philosopher:700&display=swap"),
                     .style(Style.global.cssString),
+                    .script("(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TDHD4BQ');"),
                     .viewport(.accordingToDevice)
                 ),
                 .body(
+                    .element(named: "noscript", nodes: [
+                        .raw("<iframe src=\"https://www.googletagmanager.com/ns.html?id=GTM-TDHD4BQ\"height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe>"),
+                    ]),
                     .forEach(self.body, { $0 }),
                     .siteFooter(),
                     .style(.page)
